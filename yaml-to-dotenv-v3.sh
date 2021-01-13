c2bb17d62133db74e84deceb05b0930158c9ba01
Roy <>
2021-01-13 21:06:56 +0100

    no message

--- yaml-to-dotenv-v3.sh
+++ yaml-to-dotenv-v3.sh
@@ -12,16 +12,18 @@ function handleLine() {
 #  handleMultiLine
   handleArrayLineType1
 #  handleArrayLineType2
-  handleKeyLine
+  handleValueLine
+#  handleKeyLine
 #  handleCommentLine
 }
 
 # Handle arrays with the format: ['value 1', 'value 2']
 function handleArrayLineType1() {
-  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${MULTI_LINE_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" ]]; then
+  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${MULTI_LINE_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${VALUE_OPEN}" ]]; then
     return
   fi
 
+  # START.
   if [[ "$(perl -nle 'if (/^.*?:[[:space:]]+\[/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${ARRAY_TYPE_1_OPEN}" ]]; then
     if [[ -n ${KEY_LEADING_SPACES} && "${CURRENT_LEADING_SPACES}" -le "${KEY_LEADING_SPACES}" && -n "${CURRENT_LINE}" ]]; then
       writeValueToNamespace "$(removeLeadingSpaces "${VALUE}")"
@@ -37,7 +39,7 @@ function handleArrayLineType1() {
     ARRAY_TYPE_1_OPEN=1
     ARRAY_VALUE=()
     ARRAY_INLINE=1
-  elif [[ "$(perl -nle'if (/^[[:space:]]*\[/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${ARRAY_TYPE_1_OPEN}" ]]; then
+  elif [[ "$(perl -nle 'if (/^[[:space:]]*\[/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${ARRAY_TYPE_1_OPEN}" ]]; then
     ARRAY_TYPE_1_OPEN=1
     ARRAY_VALUE=()
   fi
@@ -46,21 +48,20 @@ function handleArrayLineType1() {
     return
   fi
 
-  if [[ ARRAY_TYPE_1_OPEN -eq 1 ]]; then
-    if [[ ARRAY_INLINE -eq 1 ]]; then
-      REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^.*?[[:space:]]+\[//" | perl -pe "s/\][[:space:]]*$//")"
-    else
-      REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^[[:space:]]*\[//" | perl -pe "s/\][[:space:]]*$//" )"
-    fi
+  CURRENT_LINE_HANDLED=1
 
-    IFS=','; for EXPLODED_ITEM in ${REPLACED_LINE}; do
-      ARRAY_VALUE+=("$(trim "${EXPLODED_ITEM}")")
-    done
+  if [[ ARRAY_INLINE -eq 1 ]]; then
+    REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^.*?[[:space:]]+\[//" | perl -pe "s/\][[:space:]]*$//")"
+  else
+    REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^[[:space:]]*\[//" | perl -pe "s/\][[:space:]]*$//" )"
   fi
 
-  # reset variables as soon as we find the next key or end of document has been reached
-  # @todo add end of document check
-  if [[ ARRAY_TYPE_1_OPEN -eq 1 && "${NEXT_LEADING_SPACES}" -le "${KEY_LEADING_SPACES}" ]]; then
+  IFS=','; for EXPLODED_ITEM in ${REPLACED_LINE}; do
+    ARRAY_VALUE+=("$(trim "${EXPLODED_ITEM}")")
+  done
+
+  # END.
+  if [[ "$(perl -nle 'if (/\][[:space:]]*$/) { print $&; exit }' <<< "${CURRENT_LINE}")" || "${IS_LAST_LINE}" -eq 1 ]]; then
     if (( ${#ARRAY_VALUE[@]} )); then
       local i
       for (( i = 0; i < ${#ARRAY_VALUE[*]}; ++ i )); do
@@ -69,17 +70,14 @@ function handleArrayLineType1() {
     fi
 
     NAMESPACES=()
-
     unset ARRAY_TYPE_1_OPEN
     unset ARRAY_VALUE
     resetKeyValue
   fi
-
-  CURRENT_LINE_HANDLED=1
 }
 
 function handleArrayLineType2() {
-  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${MULTI_LINE_OPEN}" || -n "${ARRAY_TYPE_1_OPEN}" ]]; then
+  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${MULTI_LINE_OPEN}" || -n "${ARRAY_TYPE_1_OPEN}" || -n "${VALUE_OPEN}" ]]; then
     return
   fi
 
@@ -89,7 +87,7 @@ function handleArrayLineType2() {
 }
 
 function handleMultiLine() {
-  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" ]]; then
+  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${VALUE_OPEN}" ]]; then
     return
   fi
 
@@ -100,7 +98,7 @@ function handleMultiLine() {
 }
 
 function handleCommentLine() {
-  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${MULTI_LINE_OPEN}" ]]; then
+  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${MULTI_LINE_OPEN}" || -n "${VALUE_OPEN}" ]]; then
     return
   fi
 
@@ -109,26 +107,117 @@ function handleCommentLine() {
   fi
 }
 
-function handleKeyLine() {
-  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${MULTI_LINE_OPEN}" ]]; then
+function handleValueLine() {
+  if [[ "${CURRENT_LINE_HANDLED}" -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${MULTI_LINE_OPEN}" ]]; then
     return
   fi
 
-  if [[ "$(perl -nle 'if (/^.*?:/) { print $&; exit }' <<< "${CURRENT_LINE}")" ]]; then
-    if [[ -n ${KEY_LEADING_SPACES} && "${CURRENT_LEADING_SPACES}" -le "${KEY_LEADING_SPACES}" && -n "${CURRENT_LINE}" ]]; then
-      writeValueToNamespace "$(removeLeadingSpaces "${VALUE}")"
-      resetNamespace "${CURRENT_LEADING_SPACES}"
-      resetKeyValue
-    fi
+  # START.
+  if [[ "$(perl -nle 'if (/(^.*?:[[:space:]]+)?'\''/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${VALUE_OPEN}" ]]; then
+    VALUE_OPEN=1
+    VALUE_QUOTE_TYPE=1
+  elif [[ "$(perl -nle 'if (/(^.*?:[[:space:]]+)?"/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${VALUE_OPEN}" ]]; then
+    VALUE_OPEN=1
+    VALUE_QUOTE_TYPE=2
+  fi
 
-    KEY="$(echo "${CURRENT_LINE}" | perl -nle "print $& if m{^.*?:([[:space:]]+)?}" | perl -pe "s/:[[:space:]]$//")"
-    VALUE="$(echo "${CURRENT_LINE}" | perl -nle "print $& if m{:[[:space:]]+.*$}" | perl -pe "s/^:[[:space:]]//")"
-    NAMESPACES+=("${KEY}")
-    KEY_LEADING_SPACES="${CURRENT_LEADING_SPACES}"
+  if [[ VALUE_OPEN -eq 0 ]]; then
+    return
+  fi
 
-    CURRENT_LINE_HANDLED=1
+  CURRENT_LINE_HANDLED=1
+
+  if [[ -n "${VALUE}" && -n "${CURRENT_LINE}" ]]; then
+    VALUE+="\n"
+  fi
+  ### HIER GEBLEVEN TOEVOEGEN VALUE_INLINE 
+  VALUE+="$(echo "${CURRENT_LINE}" | perl -pe "s/(^.*?:)?[[:space:]]*//")"
+
+#  if [[ ARRAY_INLINE -eq 1 ]]; then
+#    REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^.*?[[:space:]]+\[//" | perl -pe "s/\][[:space:]]*$//")"
+#  else
+#    REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^[[:space:]]*\[//" | perl -pe "s/\][[:space:]]*$//" )"
+#  fi
+#
+#  IFS=','; for EXPLODED_ITEM in ${REPLACED_LINE}; do
+#    ARRAY_VALUE+=("$(trim "${EXPLODED_ITEM}")")
+#  done
+
+  # END.
+  if [[ "$(perl -nle 'if (/'\''$/) { print $&; exit }' <<< "${CURRENT_LINE}")" && VALUE_QUOTE_TYPE -eq 1 ||
+        "$(perl -nle 'if (/"$/) { print $&; exit }' <<< "${CURRENT_LINE}")" && VALUE_QUOTE_TYPE -eq 2 ||
+        "${IS_LAST_LINE}" -eq 1 ]];
+  then
+#    if (( ${#ARRAY_VALUE[@]} )); then
+#      local i
+#      for (( i = 0; i < ${#ARRAY_VALUE[*]}; ++ i )); do
+        writeValueToNamespace "${VALUE}"
+#      done
+#    fi
+
+    NAMESPACES=()
+    unset VALUE_OPEN
+    unset VALUE
+    unset VALUE_QUOTE_TYPE
+    resetKeyValue
   fi
 }
+
+#function handleKeyLine() {
+#  if [[ ${CURRENT_LINE_HANDLED} -eq 1 || -n "${ARRAY_TYPE_1_OPEN}" || -n "${ARRAY_TYPE_2_OPEN}" || -n "${MULTI_LINE_OPEN}" ]]; then
+#    return
+#  fi
+#
+#  if [[ "$(perl -nle 'if (/^.*?:/) { print $&; exit }' <<< "${CURRENT_LINE}")" ]]; then
+#    if [[ -n ${KEY_LEADING_SPACES} && "${CURRENT_LEADING_SPACES}" -le "${KEY_LEADING_SPACES}" && -n "${CURRENT_LINE}" ]]; then
+#      writeValueToNamespace "$(removeLeadingSpaces "${VALUE}")"
+#      resetNamespace "${CURRENT_LEADING_SPACES}"
+#      resetKeyValue
+#    fi
+#
+#    if [[ "$(perl -nle 'if (/^.*?:[[:space:]]+'\''/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${KEY_OPEN}" ]]; then
+#      KEY_OPEN=1
+#      KEY_QUOTE_TYPE=1
+#    elif [[  "$(perl -nle 'if (/^.*?:[[:space:]]+"/) { print $&; exit }' <<< "${CURRENT_LINE}")" && -z "${KEY_OPEN}" ]]; then
+#      KEY_OPEN=1
+#      KEY_QUOTE_TYPE=2
+#    fi
+#
+#    if [[ -n "${KEY_OPEN}" ]]; then
+##      if [[ ARRAY_INLINE -eq 1 ]]; then
+#echo "${CURRENT_LINE}"
+#      KEY_VALUE+="$(echo "${CURRENT_LINE}" | perl -pe "/[[:space:]]+looks_like_array/")"
+##        KEY_VALUE+="$(echo "${CURRENT_LINE}" | perl -pe "/^.*?:[[:space:]]+('|\")/")"
+##      else
+##        REPLACED_LINE="$(echo "${CURRENT_LINE}" | perl -pe "s/^[[:space:]]*\[//" | perl -pe "s/\][[:space:]]*$//" )"
+##      fi
+#      echo "$KEY_VALUE"
+#      exit
+##      if [[ KEY_QUOTE_TYPE -eq 1 ]]; then
+##        KEY_VALUE+=
+##      else
+##
+##      fi
+#
+#      if [[ "${NEXT_LEADING_SPACES}" -le "${KEY_LEADING_SPACES}" || -n "${IS_LAST_LINE}" ]]; then
+#        writeValueToNamespace "${KEY_VALUE}"
+#
+#        NAMESPACES=()
+#
+#        unset KEY_OPEN
+#        unset KEY_VALUE
+#        resetKeyValue
+#      fi
+#    fi
+#
+#    KEY="$(echo "${CURRENT_LINE}" | perl -nle "print $& if m{^.*?:([[:space:]]+)?}" | perl -pe "s/:[[:space:]]$//")"
+#    VALUE="$(echo "${CURRENT_LINE}" | perl -nle "print $& if m{:[[:space:]]+.*$}" | perl -pe "s/^:[[:space:]]//")"
+#    NAMESPACES+=("${KEY}")
+#    KEY_LEADING_SPACES="${CURRENT_LEADING_SPACES}"
+#
+#    CURRENT_LINE_HANDLED=1
+#  fi
+#}
 
 function resetKeyValue() {
   unset KEY
@@ -206,6 +295,18 @@ function writeValue() {
   fi
 }
 
+function isLastLine() {
+  CURRENT_LINE_NUMBER="${1}"
+  NEXT_LINE_NUMBER="$((CURRENT_LINE_NUMBER+1))"
+  TOTAL_LINES="$(wc -l "${PATH_TO_YAML_FILE}" | awk '{ print $1 }')"
+
+  if [[ "${TOTAL_LINES}" == "${NEXT_LINE_NUMBER}" ]]; then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
 rm -f  "${PATH_TO_ENV_FILE}"
 mapfile -t LINES < "${PATH_TO_YAML_FILE}"
 
@@ -214,12 +315,13 @@ if (( ${#LINES[@]} )); then
     CURRENT_LINE="${LINES[$i]}"
     CURRENT_LEADING_SPACES="$(countLeadingSpaces "${LINES[$i]}")"
     CURRENT_LINE_HANDLED=0
-    NEXT_LINE="${LINES[$i+1]}"
+#    NEXT_LINE="${LINES[$i+1]}"
     NEXT_LEADING_SPACES="$(countLeadingSpaces "${LINES[$i+1]}")"
+    IS_LAST_LINE="$(isLastLine "${i}")"
 
     handleLine
 
-    PREVIOUS_LINE="${CURRENT_LINE}"
-    PREVIOUS_LEADING_SPACES="${CURRENT_LEADING_SPACES}"
+#    PREVIOUS_LINE="${CURRENT_LINE}"
+#    PREVIOUS_LEADING_SPACES="${CURRENT_LEADING_SPACES}"
   done
 fi
